0fbd3794dc6caf4aaa77af214f58c9b714b2e123
nikai <keni1006@163.com>
2018-11-09 00:21:02 +0800

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -11,44 +11,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
-                        }
-                    },
-                    {loader: 'sass-loader'}
-                ]
-            },
-            // {
-            //     test: /\.less$/,
-            //     use: 'style!css!postcss!less'
-            // },
-            // {
-            //     test: /\.css$/,
-            //     use: ['style-loader', 'postcss-loader']
-            // },
-            // {
-            //     test: /\.ts$/,
-            //     use: 'ts-loader'
-            // }
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
         ]
     },
     mode: 'none',
-    parser: 'sugarss',
+    // parser: 'sugarss',
     plugins: [
         new htmlWebpackPlugin({
             filename: 'a.html',
